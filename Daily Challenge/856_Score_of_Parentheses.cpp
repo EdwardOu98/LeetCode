bd7d0bcddcae7e397fb355cd963d1a0c31498f63
EdwardOu98 <edward.ou1998@gmail.com>
2022-03-17 08:18:28 +0800

    Daily Challenge Mar 17, 2022

---
+++ Daily Challenge/856_Score_of_Parentheses.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> stk;
+        stk.push(0);
+        
+        for(char c : s) {
+            if(c == '(') {
+                stk.push(0);
+            }
+            else {
+                int v = stk.top();
+                stk.pop();
+                int w = stk.top();
+                stk.pop();
+                stk.push(w + max(2 * v, 1));
+            }
+        }
+        
+        return stk.top();
+    }
+};
