7cd05d390bc55a3977006eb69f97574e57a89df3
EdwardOu98 <edward.ou1998@gmail.com>
2022-04-25 10:13:38 +0800

    Practice Problems Apr 25, 2022

--- practices/Minimum_Path_Sum.cpp
+++ practices/Minimum_Path_Sum.cpp
@@ -6,27 +6,25 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        int dp[m][n];
-        memset(dp, 0, sizeof(dp));
-        dp[0][0] = grid[0][0];
-        int i, j;
-        int left, top;
-        for(i = 1; i < m; ++i) {
-            dp[i][0] = dp[i-1][0] + grid[i][0];
+        // Initialize first column
+        for(int i = 1; i < m; ++i) {
+            grid[i][0] += grid[i-1][0];
         }
-        
-        for(j = 1; j < n; ++j) {
-            dp[0][j] = dp[0][j-1] + grid[0][j];
+
+        // Initialize first row
+        for(int j = 1; j < n; ++j) {
+            grid[0][j] += grid[0][j-1];
         }
-        
-        for(i = 1; i < m; ++i) {
-            for(j = 1; j < n; ++j) {
-                top = dp[i-1][j] + grid[i][j];
-                left = dp[i][j-1] + grid[i][j];
-                dp[i][j] = min(top, left);
+
+        // For each cell, the minimum path sum to this cell
+        // is the minimum between the left and top cell plus
+        // the current cell
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
             }
         }
-        
-        return dp[m-1][n-1];
+
+        return grid[m-1][n-1];
     }
 };
--- practices/Unique_Paths.cpp
+++ practices/Unique_Paths.cpp
@@ -4,22 +4,30 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int dp[m][n];
-        int i, j;
-        
-        for(i = 0; i < m; ++i) {
+
+        // For cells in the first column, there's only
+        // one path to get to them, which is from their
+        // top cells
+        for(int i = 0; i < m; ++i) {
             dp[i][0] = 1;
         }
-        
-        for(j = 0; j < n; ++j) {
+
+        // For cells in the first row, there's only
+        // one path to get to them, which is from their
+        // left cells
+        for(int j = 0; j < n; ++j) {
             dp[0][j] = 1;
         }
-        
-        for(i = 1; i < m; ++i) {
-            for(j = 1; j < n; ++j) {
+
+        // For other cells, the number of paths to reach
+        // them is sum of the paths to reach their top
+        // and left cells
+        for(int i = 1; i < m; ++i) {
+            for(int j = 1; j < n; ++j) {
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        
+
         return dp[m-1][n-1];
     }
 };
