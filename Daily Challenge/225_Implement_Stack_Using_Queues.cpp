41ddd41320a9160f0193d3a1d7e54ff30be4dc94
EdwardOu98 <edward.ou1998@gmail.com>
2022-05-05 08:17:09 +0800

    Daily Challenge May 5, 2022

---
+++ Daily Challenge/225_Implement_Stack_Using_Queues.cpp
@@ -0,0 +1,46 @@
+#include <queue>
+
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+        // If, after pushing x into the queue, it contains
+        // n elements, move the n - 1 elements in the front
+        // to the back of the queue
+        for(int i = 0; i < q.size() - 1; ++i) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int ret = q.front();
+        q.pop();
+        return ret;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
