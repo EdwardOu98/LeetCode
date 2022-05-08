abf8a90c150d9ae764e9e395adcb4f16c869d506
EdwardOu98 <edward.ou1998@gmail.com>
2022-05-08 09:33:01 +0800

    Daily Challenge May 8, 2022

---
+++ Daily Challenge/341_Flatten_Nested_List_Iterator.cpp
@@ -0,0 +1,82 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+private:
+    stack<NestedInteger> nodes; // Stores the nodes in the nested list
+    
+    // nestedLisr - Input list
+    // n - length of input list
+    void placeNodes(vector<NestedInteger>& nestedList, int n) {
+        for(int i = n - 1; i >= 0; --i) {
+            nodes.push(nestedList[i]);
+        }
+    }
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        int n = nestedList.size();
+        // Adding the nodes in the list to the stack backwards so that when
+        // we access the nodes they are in the forward direction
+        placeNodes(nestedList, n);
+    }
+    
+    int next() {
+        // Get the element at the top of the stack
+        int res = nodes.top().getInteger();
+        // Remove the top element from the stack
+        nodes.pop();
+        // Return the element
+        return res;
+    }
+    
+    bool hasNext() {
+        // The loop is valid only when the stack is not empty
+        while(!nodes.empty()) {
+            NestedInteger curr = nodes.top();
+            
+            // If the current top element is an integer, then we can directly
+            // access its value in the next() function
+            if(curr.isInteger()) {
+                return true;
+            }
+            
+            // If the current top element is another nested list, we need to
+            // flatten it.
+            vector<NestedInteger>& nestedList = curr.getList();
+            // Remove the top element in the stack
+            nodes.pop();
+            // Add the new list nodes to the stack
+            int n = nestedList.size();
+            placeNodes(nestedList, n);
+        }
+        
+        // When the stack is empty, there's no next element in the nested list
+        return false;
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
