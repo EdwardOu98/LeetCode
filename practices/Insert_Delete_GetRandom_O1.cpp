d2e529d335ac5b4278b2c6c68c5522146119850d
EdwardOu98 <edward.ou1998@gmail.com>
2022-05-31 20:55:24 +0800

    Practice Problem May 31, 2022

--- practices/Insert_Delete_GetRandom_O1.cpp
+++ practices/Insert_Delete_GetRandom_O1.cpp
@@ -6,41 +6,46 @@ using namespace std;
 
 class RandomizedSet {
 private:
-    vector<int> nums; // 用于顺序存储输入的数字以方便随机存取
-    unordered_map<int, int> mp; // 用于存储输入的数字和对应的下标
+    vector<int> nums;
+    unordered_map<int, int> umap;
 public:
-    /** Initialize your data structure here. */
     RandomizedSet() {
-
+        
     }
     
-    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        // 如果待插入的值已经存在，则返回false
-        if(mp.count(val)) {
+        // If the input value exists in the set, return false
+        // because the insertion failed
+        if(umap.count(val)) {
             return false;
         }
-        nums.push_back(val); // 将数字插入数组的末尾，此操作不涉及元素移动，故时间复杂度为O(1)
-        mp[val] = nums.size() - 1;
+        
+        nums.push_back(val);
+        umap[val] = nums.size() - 1;
         return true;
     }
     
-    /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        // 如果待删除的值不存在于数组中，则返回false
-        if(!mp.count(val)) {
+        // If the input value doesn't exist in the set, return
+        // false because we can't delete a value that doesn't exist
+        if(!umap.count(val)) {
             return false;
         }
-        int last = nums.back(); // 取出数组中的最后一个数
-        mp[last] = mp[val]; // 交换最后一个数和待删除数字的下标
-        nums[mp[val]] = last; // 用数组的最后一个数字覆盖待删除数字
-        nums.pop_back(); // 删除数组的最后一位
-        mp.erase(val); // 删除map中待删除数字的对应项
-
+        
+        // Get the last element in the array
+        int last = nums.back();
+        // Copy the last element to the position where
+        // the deleted element is located
+        umap[last] = umap[val];
+        nums[umap[val]] = last;
+        // Remove the last element
+        nums.pop_back();
+        // Remove the input value from the map
+        umap.erase(val);
+        
         return true;
     }
     
-    /** Get a random element from the set. */
     int getRandom() {
         return nums[rand() % nums.size()];
     }
