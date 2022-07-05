d410057f091b60ec7efa6e945de2d06df29d0ede
EdwardOu98 <edward.ou1998@gmail.com>
2022-07-05 23:40:10 +0800

    Daily Challenge Jul 5, 2022

---
+++ Daily Challenge/128_Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> uset(nums.begin(), nums.end());
+        int res = 0, count;
+        int currNum;
+        int l, r;
+        
+        while(!uset.empty()) {
+            currNum = *uset.begin();
+            uset.erase(currNum);
+            l = currNum - 1, r = currNum + 1;
+            count = 1;
+            while(uset.count(l)) {
+                ++count;
+                uset.erase(l--);
+            }
+            
+            while(uset.count(r)) {
+                ++count;
+                uset.erase(r++);
+            }
+            
+            res = max(res, count);
+        }
+        
+        return res;
+    }
+};
