490fe0b6566be4c2b33c40041c2bb0b03550d72e
EdwardOu98 <edward.ou1998@gmail.com>
2022-08-06 19:52:51 +0800

    Daily Challenge Aug 6, 2022

---
+++ Daily Challenge/458_Poor_Pigs.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        // min_pig_count determined by equation: buckets =max_sub_job_load ** min_pig_count
+		// max_sub_job_load  = minutesToTest / minutesToDie + 1
+        // min_pig_count = ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
