bfb17b2872af69f3fa9a55e126fe68aa6d07ac0d
EdwardOu98 <edward.ou1998@gmail.com>
2022-12-04 09:08:18 +0800

    Daily Challenge Dec 1-4, 2022

---
+++ Daily Challenge/451_Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        int counts[256] = {0};
+        
+        for(char c : s){
+            counts[c]++;
+        }
+        
+        sort(s.begin(), s.end(), [&](char a, char b){
+            return counts[a] > counts[b] || (counts[a] == counts[b] && a < b);
+        });
+        
+        return s;
+    }
+};
